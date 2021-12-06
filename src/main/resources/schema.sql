7c0f4f0cbd20bc1f1367e4b10be5b8caf1906901
Slava <blinov.slava@list.ru>
2021-12-07 00:57:09 +0300

    В таблицы Organization и Office добавил поля "is_active"

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,50 +1,53 @@
 CREATE TABLE IF NOT EXISTS Organization (
-    id         INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    version    INTEGER NOT NULL     COMMENT 'Служебное поле hibernate',
+    id         INTEGER     NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    version    INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
     name       VARCHAR(50) NOT NULL COMMENT 'Название организации',
     full_name  VARCHAR(50) NOT NULL COMMENT 'Полное название',
     inn        VARCHAR(50) NOT NULL COMMENT 'ИНН',
     kpp        VARCHAR(50) NOT NULL COMMENT 'КПП',
     address    VARCHAR(50) NOT NULL COMMENT 'Адрес',
-    phone      VARCHAR(10) NOT NULL COMMENT 'Номер телефона'
+    phone      VARCHAR(11)          COMMENT 'Номер телефона',
+    is_active  boolean              COMMENT 'Статус активности'
 );
 COMMENT ON TABLE Organization IS 'Организация';
 
 
 CREATE TABLE IF NOT EXISTS Office (
-    id         INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    id         INTEGER     NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
     version    INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
     name       VARCHAR(50) NOT NULL COMMENT 'Название офиса',
     org_id     INTEGER     NOT NULL COMMENT 'Номер организации',
     address    VARCHAR(50) NOT NULL COMMENT 'Адрес',
-    phone      VARCHAR(10) NOT NULL COMMENT 'Номер телефона'
+    phone      VARCHAR(11)          COMMENT 'Номер телефона',
+    is_active  boolean              COMMENT 'Статус активности'
 );
 COMMENT ON TABLE Office IS 'Офис';
 
 CREATE TABLE IF NOT EXISTS Usr (
-    id         INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    id         INTEGER     NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
     version    INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
     first_name VARCHAR(50) NOT NULL COMMENT 'Имя',
-    middle_name VARCHAR(50) NOT NULL COMMENT 'Отчество',
-    last_name  VARCHAR(50) NOT NULL COMMENT 'Фамилия',
+    middle_name VARCHAR(50)         COMMENT 'Отчество',
+    last_name  VARCHAR(50)          COMMENT 'Фамилия',
     position   VARCHAR(50) NOT NULL COMMENT 'Должность',
-    phone      VARCHAR(10) NOT NULL COMMENT 'Номер телефона',
-    office_id  INTEGER     NOT NULL COMMENT 'Уникальный идентификатор офиса',
-    citizenship_id INTEGER NOT NULL COMMENT 'Уникальный идентификатор гражданства',
-    doc_number VARCHAR(50) NOT NULL COMMENT 'Номер документа'
+    phone      VARCHAR(11)          COMMENT 'Номер телефона',
+    office_id  INTEGER              COMMENT 'Уникальный идентификатор офиса',
+    citizenship_id INTEGER          COMMENT 'Уникальный идентификатор гражданства',
+    is_identified boolean           COMMENT 'Статус идентификации'
 );
 COMMENT ON TABLE Usr IS 'Пользователь';
 
 CREATE TABLE IF NOT EXISTS Document_info (
-    doc_number VARCHAR(50) NOT NULL     COMMENT 'Номер документа' PRIMARY KEY,
+    user_id    INTEGER                  COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
     version    INTEGER     NOT NULL     COMMENT 'Служебное поле hibernate',
+    doc_number VARCHAR(50) NOT NULL     COMMENT 'Номер документа',
     doc_date   VARCHAR(50) NOT NULL     COMMENT 'Дата выдачи',
     doc_name   VARCHAR(50) NOT NULL     COMMENT 'Наименование документа'
 );
 COMMENT ON TABLE Document_info IS 'Данные документа';
 
 CREATE TABLE IF NOT EXISTS Document_type (
-    doc_name   VARCHAR(50)  NOT NULL     COMMENT 'Наименование документа' PRIMARY KEY,
+    doc_name   VARCHAR(50)  NOT NULL     COMMENT 'Наименование документа'PRIMARY KEY ,
     version    INTEGER      NOT NULL     COMMENT 'Служебное поле hibernate',
     doc_code   VARCHAR(2)   NOT NULL     COMMENT 'Код документа'
 );
@@ -63,9 +66,10 @@ ALTER TABLE Office ADD FOREIGN KEY (org_id) REFERENCES Organization(id);
 
 ALTER TABLE Usr ADD FOREIGN KEY (office_id) REFERENCES Office(id);
 
-ALTER TABLE Usr ADD FOREIGN KEY (citizenship_id) REFERENCES Citizenship(citizenship_code);
+ALTER TABLE Usr ADD FOREIGN KEY (citizenship_id) REFERENCES Citizenship(id);
 
-ALTER TABLE Usr ADD FOREIGN KEY (doc_number) REFERENCES Document_info(doc_number);
+ALTER TABLE Document_info ADD FOREIGN KEY (user_id) REFERENCES Usr(id);
 
 ALTER TABLE Document_info ADD FOREIGN KEY (doc_name) REFERENCES Document_type(doc_name);
+
 
