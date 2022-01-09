0e54bfb41beb4b22f4c27d8be615db3ace76634c
Slava <blinov.slava@list.ru>
2022-01-09 16:04:50 +0300

    Добавил метод getUserFromUpdateDto и конструктор

--- src/main/java/ru/bellintegrator/practice/user/dto/UserUpdateDto.java
+++ src/main/java/ru/bellintegrator/practice/user/dto/UserUpdateDto.java
@@ -1,5 +1,6 @@
 package ru.bellintegrator.practice.user.dto;
 
+import ru.bellintegrator.practice.user.model.User;
 import javax.validation.constraints.NotEmpty;
 
 public class UserUpdateDto {
@@ -17,7 +18,27 @@ public class UserUpdateDto {
     public String docNumber;
     public String docDate;
     public String citizenshipCode;
-    public boolean isIdentifier;
+    public boolean isIdentified;
 
+    public UserUpdateDto(Integer id, Integer officeId, String firstName, String secondName, String middleName, String position, String phone, String docName, String docNumber, String docDate, String citizenshipCode, boolean isIdentified) {
+        this.id = id;
+        this.officeId = officeId;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.middleName = middleName;
+        this.position = position;
+        this.phone = phone;
+        this.docName = docName;
+        this.docNumber = docNumber;
+        this.docDate = docDate;
+        this.citizenshipCode = citizenshipCode;
+        this.isIdentified = isIdentified;
+    }
 
+    public static UserUpdateDto getUserFromUpdateDto(User user) {
+        return new UserUpdateDto(user.getId(),user.getOffice().getId(), user.getFirstName(),user.getLastName(),
+                user.getMiddleName(),user.getPosition(),user.getPhone(), user.getDocumentInfo().getDocName(),
+                user.getDocumentInfo().getDocNumber(),user.getDocumentInfo().getDocDate(),user.getCitizenship().getCitizenshipCode(),
+                user.isIdentified());
+    }
 }
