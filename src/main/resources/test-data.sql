c13c2380a1d521c0a2ade1da171637d7bd4dcd49
Slava <blinov.slava@list.ru>
2022-01-09 16:18:41 +0300

    Создал версию schema.sql и data.sql для тестов

---
+++ src/main/resources/test-data.sql
@@ -0,0 +1,28 @@
+INSERT INTO Organization (
+     version, name, full_name, inn, kpp, address, phone, is_active)
+VALUES ( 0, 'Polo', 'US POLO', '111', '222', 'Msk', '8495000000', true),
+( 0, 'Sber', 'Sber', '111', '222', 'Msk', '8495000000', true),
+( 0, 'Polo', 'US POLO', '111', '222', 'Msk', '8495000000', true);
+
+INSERT INTO Office (
+     version, name, org_id, address, phone, is_active)
+VALUES ( 0,'Fest', 1, 'Ozernaya', '8495000002', true);
+
+INSERT INTO Citizenship (
+     citizenship_code, version, citizenship_name)
+VALUES ( '633', 0, 'РФ');
+
+INSERT INTO Usr (
+    version, first_name, middle_name, last_name, position, phone, office_id, citizenship_id,is_identified )
+VALUES ( 0, 'Пётр','Петрович','Петров', 'store manager', '8909999999',2, 1,true);
+
+INSERT INTO Document_type (
+    doc_name, version, doc_code)
+VALUES ('Passport', 0, 20);
+
+INSERT INTO Document_info (
+     doc_number, version, doc_date, doc_name)
+VALUES (3315222555, 0, '14 APRIL 2001', 'Passport');
+
+
+
