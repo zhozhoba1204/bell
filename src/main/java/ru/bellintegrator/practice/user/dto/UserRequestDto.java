3ca5b54cf38e6e37157081f8d76993736971704c
Slava <blinov.slava@list.ru>
2022-01-09 16:05:43 +0300

    Изменил тип docCode на Integer

--- src/main/java/ru/bellintegrator/practice/user/dto/UserRequestDto.java
+++ src/main/java/ru/bellintegrator/practice/user/dto/UserRequestDto.java
@@ -9,6 +9,6 @@ public class UserRequestDto {
       public String lastName;
       public String middleName;
       public String position;
-      public String docCode;
+      public Integer docCode;
       public String citizenshipCode;
 }
