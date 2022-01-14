fa6100dc0b922a63e7619b39ef91e5432edf14c9
Slava <blinov.slava@list.ru>
2022-01-14 18:19:00 +0300

    Добавил записи в таблицы Organization и Office

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,26 +1,23 @@
-INSERT INTO Organization (
-        id, version, name, full_name, inn, kpp, address, phone, is_active)
-        VALUES (1, 0, 'Polo', 'US POLO', '111', '222', 'Msk', '84950000001', true);
+INSERT INTO Organization (id, version, name, full_name, inn, kpp, address, phone, is_active)
+VALUES (1, 0, 'Polo', 'US POLO', '111', '222', 'Msk', '84950000001', true),
+       (2, 0, 'sber', 'sberbank', '111', '222', 'Msk', '84950000001', false);
 
-INSERT INTO Office (
-        id, version, name, org_id, address, phone, is_active)
-        VALUES (2, 0,'Fest', 1, 'Ozernaya', '84950000002', true);
+INSERT INTO Office (id, version, name, org_id, address, phone, is_active)
+VALUES (2, 0, 'Fest', 1, 'Ozernaya', '84950000002', true),
+       (3, 0, 'Rio', 1, 'Ozerka', '84950000002', false);
 
-INSERT INTO Citizenship (
-    id, citizenship_code, version, citizenship_name)
+INSERT INTO Citizenship (id, citizenship_code, version, citizenship_name)
 VALUES (1, '633', 0, 'РФ');
 
-INSERT INTO Usr (
-        id, version, first_name, middle_name, last_name, position, phone, office_id, citizenship_id,is_identified )
-        VALUES (1, 0, 'Пётр','Петрович','Петров', 'store manager', '89099999999',2, 1,true);
+INSERT INTO Usr (id, version, first_name, middle_name, last_name, position, phone, office_id, citizenship_id,
+                 is_identified)
+VALUES (1, 0, 'Пётр', 'Петрович', 'Петров', 'store manager', '89099999999', 2, 1, true);
 
-INSERT INTO Document_type (
-    doc_name, version, doc_code)
+INSERT INTO Document_type (doc_name, version, doc_code)
 VALUES ('Паспорт', 0, 20);
 
-INSERT INTO Document_info (
-    user_id, doc_number, version, doc_date, doc_name)
-    VALUES (1,3315222555, 0, '14 APRIL 2001', 'Паспорт');
+INSERT INTO Document_info (user_id, doc_number, version, doc_date, doc_name)
+VALUES (1, 3315222555, 0, '14 APRIL 2001', 'Паспорт');
 
 
 
