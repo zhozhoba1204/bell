4af46f392615aaca93b0eded735b098764412557
Slava <blinov.slava@list.ru>
2022-01-16 16:15:46 +0300

    Добавил конструктор и геттеры/сеттеры

--- src/main/java/ru/bellintegrator/practice/office/dto/OfficeUpdateDto.java
+++ src/main/java/ru/bellintegrator/practice/office/dto/OfficeUpdateDto.java
@@ -16,6 +16,11 @@ public class OfficeUpdateDto {
     public OfficeUpdateDto() {
     }
 
+    public OfficeUpdateDto(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public OfficeUpdateDto(Integer id, String name, String address, String phone, boolean isActive) {
         this.id = id;
         this.name = name;
@@ -24,8 +29,44 @@ public class OfficeUpdateDto {
         this.isActive = isActive;
     }
 
-    public static OfficeUpdateDto getOfficeFromUpdateDto(Office office){
+    public static OfficeUpdateDto getUpdateDtoFromOffice(Office office){
         return new OfficeUpdateDto(office.getId(), office.getName(), office.getAddress(),
                 office.getPhone(), office.asActive());
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean asActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean active) {
+        isActive = active;
     }
 }
