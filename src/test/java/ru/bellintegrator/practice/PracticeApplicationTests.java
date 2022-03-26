909e114284a4aaec094f06d289a88719f3334d8d
Slava <blinov.slava@list.ru>
2022-03-26 17:14:22 +0300

    add swagger

---
+++ src/test/java/ru/bellintegrator/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bellintegrator.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
